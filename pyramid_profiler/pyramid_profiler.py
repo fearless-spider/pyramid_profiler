4b9536d48f88d553218969b53750f671292280c4
Przemysław Pająk <office@fearlessspider.com>
2022-07-25 18:22:39 +0200

    Added Measurement class

--- pyramid_profiler/pyramid_profiler.py
+++ pyramid_profiler/pyramid_profiler.py
@@ -10,4 +10,43 @@ import logging
 
 from . import storage
 
-logger = logging.getLogger("pyramid-profiler")
+logger = logging.getLogger("pyramid_profiler")
+
+
+class Measurement(object):
+    """represents an endpoint measurement"""
+    DECIMAL_PLACES = 6
+
+    def __init__(self, name, args, kwargs, method, context=None):
+        super(Measurement, self).__init__()
+        self.context = context
+        self.name = name
+        self.method = method
+        self.args = args
+        self.kwargs = kwargs
+        self.startedAt = 0
+        self.endedAt = 0
+        self.elapsed = 0
+
+    def __json__(self):
+        return {
+            "name": self.name,
+            "args": self.args,
+            "kwargs": self.kwargs,
+            "method": self.method,
+            "startedAt": self.startedAt,
+            "endedAt": self.endedAt,
+            "elapsed": self.elapsed,
+            "context": self.context
+        }
+
+    def __str__(self):
+        return str(self.__json__())
+
+    def start(self):
+        self.startedAt = time.time()
+
+    def stop(self):
+        self.endedAt = time.time()
+        self.elapsed = round(
+            self.endedAt - self.startedAt, self.DECIMAL_PLACES)
